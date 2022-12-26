470079517617d622edba2d656b4ec344e08b64a5
Jeremy Smith <st.jeremy1@gmail.com>
2022-12-26 23:48:27 +0100

    fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import { Provider } from 'react-redux';
+import store from '../redux/store';   
 import type { AppProps } from 'next/app'
 import Header from '../Components/Header'
 import { Box, ChakraProvider } from '@chakra-ui/react'
@@ -6,12 +8,14 @@ import Footer from '../Components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Box minHeight={'150vh'} display={'flex'} flexDirection={'column'} justifyContent={'space-between'}>
-      <ChakraProvider>
-        <Header />
-          <Component {...pageProps} />
-        <Footer />
-      </ChakraProvider>
-    </Box>
+    <Provider store={store}>
+      <Box minHeight={'150vh'} display={'flex'} flexDirection={'column'} justifyContent={'space-between'}>
+        <ChakraProvider>
+          <Header />
+            <Component {...pageProps} />
+          <Footer />
+        </ChakraProvider>
+      </Box>
+    </Provider>
   )
 }
