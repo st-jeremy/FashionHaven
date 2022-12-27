d4dbcbf8c4785db2a2f429ef5e828dd3e87c0b1c
Jeremy Smith <st.jeremy1@gmail.com>
2022-12-27 19:21:06 +0100

    fix

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,5 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { cartReducer } from './cart.slice';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
 const reducer = {
   cart: cartReducer,
