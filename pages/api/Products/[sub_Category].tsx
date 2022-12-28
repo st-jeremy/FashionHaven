23e45b2cb30462c94b5061d8e5115ce841dbf3e8
Jeremy Smith <st.jeremy1@gmail.com>
2022-12-28 19:08:49 +0100

    fix

---
+++ pages/api/Products/[sub_Category].tsx
@@ -0,0 +1,19 @@
+import data from './data.json';
+
+export function getProductsBySubCategory(sub_Category) {
+  const products = data.filter((product) => product.sub_Category === sub_Category);
+  return products;
+}
+
+
+export default function handler(req, res) {
+
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).json({ message: `Method ${req.method} is not allowed` });
+  } else {
+    const products = getProductsBySubCategory(req.query.sub_Category);
+    res.status(200).json(products);
+  }
+}
+
