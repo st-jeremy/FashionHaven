4b632e56fc17c6e139754859b5d430aeb5187d9e
Jeremy Smith <st.jeremy1@gmail.com>
2022-12-29 22:55:41 +0100

    fix

--- Components/Value.tsx
+++ Components/Value.tsx
@@ -5,34 +5,39 @@ import { AiOutlineSafety } from 'react-icons/ai'
 import { Box, Heading } from "@chakra-ui/react";
 
 const Value = () => {
-  
+
   return ( 
-    <Box display={'flex'} textAlign={'center'} p={5} mt={20} maxWidth={'1400px'} margin={'auto'}>
-      <Box >
-        <BiDollar style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
-        <br />
-        <Heading size='md'>Great Value</Heading>
-        <p>We offer competitive prices on over 100 million items.</p>
+    <Box display={'flex'} flexDirection={{base: 'column', lg: 'row'}} textAlign={'center'} p={5} mt={20} maxWidth={'1400px'} margin={'auto'} >
+      <Box display={{base: 'block', md: 'flex'}} mb={7}>
+        <Box mr={3}>
+          <BiDollar style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
+          <br />
+          <Heading size='md'>Great Value</Heading>
+          <p>We offer competitive prices on over 100 million items.</p>
+        </Box>
+        <Box mr={3}>
+          <TbTruckDelivery style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}} />
+          <Heading  size='md'>Worldwide Delivery</Heading>
+          <p>We ship to all continents of the world.</p>
+        </Box>
+        <Box>
+          <BsCreditCard2Back  style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
+          <Heading  size='md'>Safe payment</Heading>
+          <p>Pay with the world’s most popular and secure payment methods.</p>
+        </Box>
       </Box>
-      <Box>
-        <TbTruckDelivery style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}} />
-        <Heading  size='md'>Worldwide Delivery</Heading>
-        <p>We ship to all continents of the world.</p>
-      </Box>
-      <Box>
-        <BsCreditCard2Back  style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
-        <Heading  size='md'>Safe payment</Heading>
-        <p>Pay with the world’s most popular and secure payment methods.</p>
-      </Box>
-      <Box>
-        <AiOutlineSafety style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
-        <Heading  size='md'>Shop with confidence</Heading>
-        <p>Our Buyer Protection policy covers your entire purchase journey.</p>
-      </Box>
-      <Box>
-        <BsFillPeopleFill style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
-        <Heading  size='md'>Help center</Heading>
-        <p>Round-the-clock assistance for a smooth shopping experience.</p>
+
+      <Box display={{base: 'block', md: 'flex'}} >
+        <Box mr={3}>
+          <AiOutlineSafety style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
+          <Heading  size='md'>Shop with confidence</Heading>
+          <p>Our Buyer Protection policy covers your entire purchase journey.</p>
+        </Box>
+        <Box mr={3}>
+          <BsFillPeopleFill style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
+          <Heading  size='md'>Help center</Heading>
+          <p>Round-the-clock assistance for a smooth shopping experience.</p>
+        </Box>
       </Box>
     </Box>
    );
