0186ed58ee8b41802ebc466dffb1458fca6eadca
Jeremy Smith <st.jeremy1@gmail.com>
2022-12-29 10:59:39 +0100

    fiv

--- Components/CategoryCard.tsx
+++ Components/CategoryCard.tsx
@@ -1,58 +1,21 @@
 import { 
-  Box, 
-  Heading,
+  Box,
   Button,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  List,
-  ListItem,
-  ListIcon,
 } from "@chakra-ui/react";
 import Link from 'next/link';
-import { MdCheckCircle } from 'react-icons/md'
 
 const CategoryCard = ({ name }) => {
   return ( 
     <Box>
-      <Accordion  
-        // defaultIndex={[0]}  
-        allowToggle 
+      <Button
+        size='md'
+        height='48px'
+        width='200px'
+        border='2px'
+        variant='solid'
       >
-        <AccordionItem>
-          <Heading>
-            <AccordionButton>
-            <Button
-              size='md'
-              height='48px'
-              width='200px'
-              border='2px'
-              variant='solid'
-            >
-              <Link href={`/category/${name}`}>{name}</Link>
-              <AccordionIcon  style={{position:'absolute', right: '10'}}/>
-            </Button>
-            </AccordionButton>
-          </Heading>
-
-          <AccordionPanel pb={4} pl={6} color={'white'}>
-            <List spacing={3}>
-              <ListItem>
-                <ListIcon as={MdCheckCircle} color='green.500' />
-                Male
-              </ListItem>
-              <ListItem>
-                <ListIcon as={MdCheckCircle} color='green.500' />
-                Female
-              </ListItem>
-              </List>
-            </AccordionPanel>
-
-          </AccordionItem>
-        </Accordion>
-      
+        <Link href={`/category/${name}`}>{name}</Link>
+      </Button>
     </Box>
    );
 }
