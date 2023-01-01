932395e1c3e54312bf4cd9f37996a77ca17919e5
Jeremy Smith <st.jeremy1@gmail.com>
2023-01-01 22:18:15 +0100

    fix

--- Components/CartIcon.tsx
+++ Components/CartIcon.tsx
@@ -1,11 +1,24 @@
 import { useRouter } from 'next/router';
+import { AvatarBadge, Avatar  } from '@chakra-ui/react'
+import { useSelector } from 'react-redux';
+import { State } from './Types';
 
 const CartIcon = () => {
 
   const router = useRouter();
   const handleCartClick =() =>{
     router.push('/Cart')
-  }
+  };
+
+   // Selecting cart from global state
+   const cart = useSelector((state: State) => state.cart);
+
+   // Getting the count of items
+  const getItemsCount = () => {
+    return (
+      cart.reduce((accumulator, item) => accumulator + item.quantity, 0)
+    )
+   };
   return ( 
   <>
     <Avatar 
