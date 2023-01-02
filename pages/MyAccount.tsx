8648e55673ad41d08434b4ab7fe569413e362755
Jeremy Smith <st.jeremy1@gmail.com>
2023-01-02 17:05:28 +0100

    fixes

--- pages/MyAccount.tsx
+++ pages/MyAccount.tsx
@@ -8,8 +8,8 @@ const MyAccount = () => {
 
   return ( 
     <Box>
-      <Image src={session?.user?.image} alt={session.user.name} width={200} height={200}  />
-      <Text>{session.user?.name}</Text>
+      <Image src={session.user.image} alt={session.user.name} width={200} height={200}  />
+      <Text>{session.user.name}</Text>
       <Text>{session.user.email}</Text>
     </Box>
    );
