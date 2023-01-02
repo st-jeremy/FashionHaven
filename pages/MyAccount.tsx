b531dad00a7aeaa542f5b0f018e522aaa0250253
Jeremy Smith <st.jeremy1@gmail.com>
2023-01-02 11:21:14 +0100

    Myaccount page

--- pages/MyAccount.tsx
+++ pages/MyAccount.tsx
@@ -1,9 +1,13 @@
 import { Box } from "@chakra-ui/react";
+import { useSession } from "next-auth/react"
+import Image from "next/image";
 
 const MyAccount = () => {
+
+  const { data: session } = useSession()
   return ( 
     <Box>
-      
+      <Image src={session.user.image} alt={session.user.name} width={200} height={200}  />
     </Box>
    );
 }
