3436fbec63f61528e9ae52ab8f3402289493a700
Jeremy Smith <st.jeremy1@gmail.com>
2023-01-02 17:16:05 +0100

    fixes

--- pages/MyAccount.tsx
+++ pages/MyAccount.tsx
@@ -2,15 +2,20 @@ import { Box, Text } from "@chakra-ui/react";
 import { useSession } from "next-auth/react"
 import Image from "next/image";
 
+
 const MyAccount = () => {
 
   const { data: session } = useSession();
 
+  const userEmail = session?.user.email;
+  const userName = session?.user.name;
+  const userImage = session?.user.image
+
   return ( 
     <Box>
-      <Image src={session.user?.image} alt={session.user?.name} width={200} height={200}  />
-      <Text>{session.user?.name}</Text>
-      <Text>{session.user?.email}</Text>
+      <Image src={userImage} alt={userName} width={200} height={200}  />
+      <Text>{userName}</Text>
+      <Text>{userEmail}</Text>
     </Box>
    );
 }
