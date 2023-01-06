4af60cd1473291ad0cd191d85420aa0e7ec4fc9a
Jeremy Smith <st.jeremy1@gmail.com>
2023-01-06 14:41:05 +0100

    fix

--- Components/AddToCartButton.tsx
+++ Components/AddToCartButton.tsx
@@ -25,10 +25,8 @@ const AddtoCartButton = ({ product }) => {
   return ( 
     <Box>
       <Button 
-        colorScheme='red' 
         position={'absolute'} 
         right={'2.5'} 
-        p={1} 
         fontSize='sm' 
         onClick={handleClick}
       >
--- Components/ProductCard.tsx
+++ Components/ProductCard.tsx
@@ -8,34 +8,32 @@ import {
 } from '@chakra-ui/react';
 import Image from 'next/image';
 import AddtoCartButton from './AddToCartButton';
+import { BiBox } from 'react-icons/bi';
 
 const ProductCard = ({ product }) => {
 
   return (
-    <Box>
-      <Card m={5} width={250} height={250} mb={50} boxShadow={'lg'}>
+    <Card m={5} width={200} height={250} mb={50} boxShadow={'lg'}>
 
-        <Box maxHeight={200} minHeight={200} >
-          <Image src={product.image} alt={product.name} width={700} height={200} />
-        </Box>
+      <Box maxHeight={200} minHeight={200} >
+        <Image src={product.image} alt={product.name} width={200} height={250} priority />
+      </Box>
 
-        <Divider />
+      <Divider />
 
-        <Heading size='md' bgColor={'white'} textAlign={'center'} pt={1}>{product.name}</Heading>
 
-        <Text>${product.price}</Text>
-        
+      {/* <Text>${product.price}</Text> */}
+
+      {/* <AddtoCartButton product={product} /> */}
+
+      <Box bgColor={'white'} borderRadius={'0  0 13px 13px'}>
+
+        <Text size='md' bgColor={'white'} textAlign={'center'} pt={1}>{product.name}</Text>
+        <Text fontWeight={'bold'} >${product.price}</Text>
+
         <AddtoCartButton product={product} />
-
-        {/* <CardFooter bgColor={'white'} borderRadius={'0  0 13px 13px'}>
-          <Text color='blue.600' fontSize='xl' >
-            <b>${product.price}</b>
-          </Text>
-
-          <AddtoCartButton product={product} /> */}
-        {/* </CardFooter> */}
-      </Card>
-    </Box>
+      </Box>
+    </Card>
   );
 };
 
