73d3fd0e6ac13b69861a4c048acf8c422314a611
Jeremy Smith <st.jeremy1@gmail.com>
2023-01-07 11:59:59 +0100

    fix

--- Components/MultiCarousel/AllStock/index.tsx
+++ Components/MultiCarousel/AllStock/index.tsx
@@ -46,9 +46,9 @@ const AllStock = ({product}) => {
     <Box bgColor={'blackAlpha.100'} maxWidth={'1400px'} m={'auto'} mt={7} pb={10}>
 
       <Box  bgColor={'black'} display={'flex'} color={'white'} p={2.5}>
-        <Heading  fontSize={'24pt'} color={'white'}>All Stock</Heading>
+        <Heading  fontSize={'24pt'} color={'white'} >All Stock</Heading>
         <Link href='/Shop'>
-          <Button bgColor={'black'} position={'absolute'} right={'7rem'} _hover={{textDecoration:'underline', bgColor: 'white', color: 'black'}}>View All</Button>
+          <Button bgColor={'black'} position={{base: 'absolute', lg: 'relative'}} right={{base: '0', lg: '20rem'}} _hover={{textDecoration:'underline', bgColor: 'white', color: 'black'}}>View All</Button>
         </Link>
       </Box>
 
--- Components/Value.tsx
+++ Components/Value.tsx
@@ -7,9 +7,9 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 const Value = () => {
 
   return ( 
-    <Box display={'flex'} flexDirection={{base: 'column'}} textAlign={'center'} p={{base: '2.5', lg: '5'}} pt={'6rem'} pb={'6rem'} width={'fit-content'}  maxWidth={'1400px'} margin={'auto'} >
+    <Box display={'flex'} flexDirection={{base: 'column'}} textAlign={'center'} p={{base: '2.5', lg: '5'}} pt={'6rem'} pb={'6rem'} width={'fit-content'}  maxWidth={'1400px'} margin={'0'}>
 
-      <Box display={{base: 'block', md: 'flex'}} margin={{base: '0', lg: 'auto'}} mb={7} width={'90vw'}>
+      <Box display={{base: 'block', md: 'flex'}} margin={{base: '0', lg: 'auto'}} mb={7} width={{base: '93vw', sm: '97vw'}}>
         <Box mr={3}>
           <BiDollar style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
           <br />
@@ -30,14 +30,14 @@ const Value = () => {
         </Box>
       </Box>
 
-      <Box display={{base: 'block', md: 'flex'}} margin={{base: '0', lg: 'auto'}} width={'90vw'}>
-        <Box >
+      <Box display={{base: 'block', md: 'flex'}} margin={{base: '0', lg: 'auto'}} width={{base: '93vw', sm: '97vw'}} maxWidth={'1400px'}>
+        <Box  m={'auto'}>
           <AiOutlineSafety style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
           <Heading  size='md'>Shop with confidence</Heading>
           <p>Our Buyer Protection policy covers your entire purchase journey.</p>
         </Box>
 
-        <Box >
+        <Box m={'auto'}>
           <BsFillPeopleFill style={{position: 'relative', display: 'inline-flex', fontSize: '26pt',}}/>
           <Heading  size='md'>Help center</Heading>
           <p>Round-the-clock assistance for a smooth shopping experience.</p>
