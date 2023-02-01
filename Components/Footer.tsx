8bcf87e3ab57fd4c1566f36b5ac08943aa2ef8c2
Jeremy Smith <st.jeremy1@gmail.com>
2023-02-01 20:18:59 +0100

    fix

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,14 +1,9 @@
 import { Box, Heading, Input, InputGroup, InputRightAddon, Text, Button} from "@chakra-ui/react";
-import { useRouter } from "next/router";
 import { SlSocialFacebook, SlSocialLinkedin, SlSocialInstagram } from 'react-icons/sl'
 import Logo from "./Logo";
 import Link from "next/link";
 
 const Footer = () => {
-  const router = useRouter();
-  const handleHomeClick =() =>{
-    router.push('/')
-  }
 
   return ( 
     <Box  bgColor={'black'}>
@@ -40,7 +35,7 @@ const Footer = () => {
           <Box position={'relative'} mb={10} mr={10}>
             <Heading size={{base: 'sm', md: 'md'}}>Subscribe</Heading>
             <Text fontSize={{base: '12pt', sm: '14pt'}}>Subscribe to our newsletter to get updates on our latest offers!</Text>
-            <br />
+            <br />  
             <InputGroup>
               <Input type={'email'} placeholder={'Enter your email address'} width={{base: '400px', md: 'fit-content'}} />
               <InputRightAddon color={'black'} bgColor={'white'}> &rarr;</InputRightAddon>
